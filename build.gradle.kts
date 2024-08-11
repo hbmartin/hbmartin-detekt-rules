6d2f34c63794d25e9602ac97558fdbf3acce1879
Harold Martin <harold.martin@gmail.com>
2024-08-11 06:50:33 -0700

    configure maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import java.net.URI
 
 plugins {
     kotlin("jvm") version "2.0.0"
-    `maven-publish`
-    signing
     alias(libs.plugins.detekt)
     jacoco
     id("com.github.ben-manes.versions") version "0.51.0"
@@ -45,23 +44,39 @@ tasks.jacocoTestReport {
     }
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-        }
-    }
-    repositories {
-        maven {
-            name = "OSSRH"
-            url = URI("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = System.getenv("MAVEN_USERNAME")
-                password = System.getenv("MAVEN_PASSWORD")
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    coordinates("me.haroldmartin", "hbmartin-detekt-rules", version as String)
+
+    pom {
+        name.set("Hbmartin's Detekt Rules")
+        description.set("A somewhat opinionated ruleset for Detekt, primarily intended to avoid crashes and bugs related to mutability.")
+        inceptionYear.set("2023")
+        url.set("https://github.com/hbmartin/hbmartin-detekt-rules")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
             }
+        }
+        developers {
+            developer {
+                id.set("hmartin")
+                name.set("Harold Martin")
+                url.set("https://github.com/hbmartin/")
+            }
+        }
+        scm {
+            url.set("https://github.com/hbmartin/hbmartin-detekt-rules/")
+            connection.set("scm:git:git://github.com/hbmartin/hbmartin-detekt-rules.git")
+            developerConnection.set("scm:git:ssh://git@github.com/hbmartin/hbmartin-detekt-rules.git")
         }
     }
 }
+
 
 detekt {
     allRules = true
